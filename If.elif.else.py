17323561dd13ab1e8b957b1c38975570edaafb5a
Raúl Tamayo <261541580+raultamayoverdecia-hue@users.noreply.github.com>
2026-05-02 16:35:52 -0300

    If_elif_else.py

---
+++ If.elif.else.py
@@ -0,0 +1,10 @@
+idade_animal = 5 # Ejemplo de valor
+
+if idade_animal <= 1:
+    print("Filhote")
+elif idade_animal < 6:
+    print("Jovem")
+elif idade_animal < 11:
+    print("Adulto")
+else:
+    print("Idoso")
